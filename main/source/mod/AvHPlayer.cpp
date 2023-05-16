856fdca97133d142d04535d72e0888d8a60ce7bb
EterniumDev <59627760+EterniumDev@users.noreply.github.com>
2023-05-16 11:23:00 -0600

    Fixed Faded mode marine team size balance not working
    Fixed bug where Faded balance modifier for the marine didn't update their Weapon and Armor upgrades depending on player count

--- main/source/mod/AvHPlayer.cpp
+++ main/source/mod/AvHPlayer.cpp
@@ -8083,21 +8083,35 @@ void AvHPlayer::SetPlayMode(AvHPlayMode inPlayMode, bool inForceSpawn)
 					if (testTeam->GetPlayerCount() <= 8) {
 						GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_ONE, testTeam->GetTeamNumber(), 0, true);
 						GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_ONE, testTeam->GetTeamNumber(), 0, true);
-						//this->GiveTeamUpgrade(RESEARCH_WEAPONS_ONE);
-						//this->GiveTeamUpgrade(RESEARCH_ARMOR_ONE);
+
 						if (testTeam->GetPlayerCount() <= 5) {
 							GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_TWO, testTeam->GetTeamNumber(), 0, true);
 							GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_TWO, testTeam->GetTeamNumber(), 0, true);
-
 							GetGameRules()->ProcessTeamUpgrade(RESEARCH_HEALTH, testTeam->GetTeamNumber(), 0, true);
-							//this->GiveTeamUpgrade(RESEARCH_WEAPONS_TWO);
-							//this->GiveTeamUpgrade(RESEARCH_ARMOR_TWO);
+
 							this->GiveNamedItem("weapon_shotgun");
+
 							if (testTeam->GetPlayerCount() <= 3) {
 								GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_THREE, testTeam->GetTeamNumber(), 0, true);
 								GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_THREE, testTeam->GetTeamNumber(), 0, true);
 							}
+							else
+							{
+								GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_THREE, testTeam->GetTeamNumber(), 0, false);
+								GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_THREE, testTeam->GetTeamNumber(), 0, false);
+							}
 						}
+						else
+						{
+							GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_TWO, testTeam->GetTeamNumber(), 0, false);
+							GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_TWO, testTeam->GetTeamNumber(), 0, false);
+							GetGameRules()->ProcessTeamUpgrade(RESEARCH_HEALTH, testTeam->GetTeamNumber(), 0, false);
+						}
+					}
+					else
+					{
+						GetGameRules()->ProcessTeamUpgrade(RESEARCH_WEAPONS_ONE, testTeam->GetTeamNumber(), 0, false);
+						GetGameRules()->ProcessTeamUpgrade(RESEARCH_ARMOR_ONE, testTeam->GetTeamNumber(), 0, false);
 					}
 					
 					//this->GiveTeamUpgrade(RESEARCH_ELECTRICAL);
