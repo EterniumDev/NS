cfab09ac171df836c3037cecffb9d2f7a6c4e916
EterniumDev <59627760+EterniumDev@users.noreply.github.com>
2024-01-16 14:19:39 -0700

    Increased vampirism lifesteal from 10% to 15% per level

--- main/source/mod/AvHBaseBuildable.cpp
+++ main/source/mod/AvHBaseBuildable.cpp
@@ -1327,7 +1327,7 @@ int	AvHBaseBuildable::TakeDamage(entvars_t* inInflictor, entvars_t* inAttacker, 
 				if (avh_modvampirism.value != 0 && GetHasUpgrade(atkPlayer->pev->iuser4, MASK_UPGRADE_3) && (atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER1 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER2 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER3 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER4 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER5))
 				{
 					int theVampirismLevel = AvHGetAlienUpgradeLevel(atkPlayer->pev->iuser4, MASK_UPGRADE_3);
-					vamp = 0.0f + (((float)theVampirismLevel)*0.1f);
+					vamp = 0.0f + (((float)theVampirismLevel)*0.15f);
 				}
 
 				if (vamp > 0)
--- main/source/mod/AvHPlayer.cpp
+++ main/source/mod/AvHPlayer.cpp
@@ -9644,7 +9644,7 @@ int AvHPlayer::TakeDamage( entvars_t* pevInflictor, entvars_t* pevAttacker, floa
 				if (avh_modvampirism.value != 0 && GetHasUpgrade(atkPlayer->pev->iuser4, MASK_UPGRADE_3) && (atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER1 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER2 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER3 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER4 || atkPlayer->pev->iuser3 == AVH_USER3_ALIEN_PLAYER5))
 				{
 					int theVampirismLevel = AvHGetAlienUpgradeLevel(atkPlayer->pev->iuser4, MASK_UPGRADE_3);
-					vamp = 0.0f + (((float)theVampirismLevel)*0.1f);
+					vamp = 0.0f + (((float)theVampirismLevel)*0.15f);
 
 				}
 
